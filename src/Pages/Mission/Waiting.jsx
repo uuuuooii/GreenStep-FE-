61db10b7711aebac639228cb3feff602c7b97236
neo@zillinks.com <keunhey0107@gmail.com>
2022-09-01 03:49:19 +0900

    boxflex

--- src/Pages/Mission/Waiting.jsx
+++ src/Pages/Mission/Waiting.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Waiting.css";
 
 const Waiting = () => {
-  return <div>Waiting</div>;
+  return (
+    <>
+      <div>
+        {/* <div className="WaitingWrap"> */}
+        <div className="WaitingMissionBox">
+          <div className="WaitingMission">
+            <div>
+              <div className="WaitingMiniBox">
+                <p className="Waitingtext">인증 대기중</p>
+              </div>
+              <p className="WaitingMissionName">Misson Name</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* </div> */}
+    </>
+  );
 };
 
 export default Waiting;

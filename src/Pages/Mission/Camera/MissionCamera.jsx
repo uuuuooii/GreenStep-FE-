600296a00b9433258ff862109a385f47974f96d8
neo@zillinks.com <keunhey0107@gmail.com>
2022-09-03 02:17:12 +0900

    fix

--- src/Pages/Mission/Camera/MissionCamera.jsx
+++ src/Pages/Mission/Camera/MissionCamera.jsx
@@ -1,43 +1,74 @@
+//react import
 import React, { useState, useRef } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Camera } from "react-camera-pro";
 import "./MissionCamera.css";
+import { CameraBar, UploadButton, UploadImg } from "./CameraBar";
+//modules import
+import instance from "../../../Redux/modules/instance";
 
 export const MissionCamera = () => {
   const camera = useRef(null);
   const [numberOfCameras, setNumberOfCameras] = useState(0);
   const [image, setImage] = useState(null);
+  const Certification = { imgUrl: image };
+  const URL = process.env.REACT_APP_URL;
+  const missionId = useParams();
+  const navigate = useNavigate();
+  const Upload = () => {
+    instance.post(`${URL}/missions/${missionId}`, Certification);
+    navigate("/Upload");
+  };
 
   return (
     <div className="wrap">
-      <Camera
-        ref={camera}
-        aspectRatio="cover"
-        numberOfCamerasCallback={setNumberOfCameras}
-      />
-      <div className="control">
-        <div className="ImagePreview" image={image} />
-        <div
-          className="TakePhotoButton"
-          onClick={() => {
-            if (camera.current) {
-              const photo = camera.current.takePhoto();
-              console.log(photo);
-              setImage(photo);
-            }
-          }}
+      {image ? (
+        <UploadImg src={image} />
+      ) : (
+        <Camera
+          ref={camera}
+          aspectRatio="cover"
+          numberOfCamerasCallback={setNumberOfCameras}
         />
-        <div
-          className="ChangeFacingCameraButton"
-          disabled={numberOfCameras <= 1}
-          onClick={() => {
-            if (camera.current) {
-              const result = camera.current.switchCamera();
-              console.log(result);
-            }
-          }}
-        />
-      </div>
+      )}
+      {image ? (
+        <CameraBar>
+          <UploadButton onClick={Upload} />
+        </CameraBar>
+      ) : (
+        <div className="control">
+          <div className="ImagePreview" image={image} />
+          <div
+            className="TakePhotoButton"
+            onClick={() => {
+              if (camera.current) {
+                const photo = camera.current.takePhoto();
+                setImage(photo);
+              }
+            }}
+          />
+          <div
+            className="ChangeFacingCameraButton"
+            disabled={numberOfCameras <= 1}
+            onClick={() => {
+              if (camera.current) {
+                const result = camera.current.switchCamera();
+                console.log(result);
+              }
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 };
 export default MissionCamera;
+
+// const ImagePreview = styled.div`
+//   width: 120px;
+//   height: 120px;
+//   ${({ image }) => (image ? `background-image:  url(${image});` : "")}
+//   background-size: contain;
+//   background-repeat: no-repeat;
+//   background-position: center;
+// `;

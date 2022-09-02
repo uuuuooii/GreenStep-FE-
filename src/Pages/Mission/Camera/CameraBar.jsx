a6cfc722703c440e5912547702d3a911ef6c3090
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-09-03 03:39:24 +0900

    [feat]missionpage

--- src/Pages/Mission/Camera/CameraBar.jsx
+++ src/Pages/Mission/Camera/CameraBar.jsx
@@ -1,22 +1,23 @@
+//styled import
 import { BsFillArrowUpCircleFill } from 'react-icons/bs';
 import styled from 'styled-components';
 
 export const CameraBar = styled.div`
-width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const UploadButton = styled(BsFillArrowUpCircleFill)`
-    width: 35px;
-    height: 35px;
-    margin: 15px 0;
-    &:hover{
-        cursor: pointer;
-    }
-`
+  width: 35px;
+  height: 35px;
+  margin: 15px 0;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 export const UploadImg = styled.img`
-    width: 400px;
-    height: 400px;
-`
+  width: 400px;
+  height: 400px;
+`;

5ad6639dedc04cdc07071ac7e59f1df537b13493
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-09-03 23:56:58 +0900

    [feat]explain

--- src/Pages/Mission/Explain/Explain.jsx
+++ src/Pages/Mission/Explain/Explain.jsx
@@ -1,15 +1,17 @@
+//react import
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Explain.css';
-import { FiCamera } from 'react-icons/fi';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+//styled import
+import './Explain.css';
+import { FiCamera } from 'react-icons/fi';
 import {
   __GetTodaymission,
   __GetDailymission,
 } from '../../../Redux/modules/mission';
 const Explain = () => {
-  const [loding,setLoding] = useState(false)
+  const [loding, setLoding] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const paramsNum = useParams().id.split('&')[0];
@@ -20,30 +22,35 @@ const Explain = () => {
       : paramsCategory === 'daily'
       ? state.mission.daily.filter((item) => item.missionId == paramsNum)[0]
       : state.mission.weekly.filter((item) => item.missionId == paramsNum)[0]
-  )
-  // .filter((item) => item.missionId == paramsNum)[0]
-
-  // const text = select.map((item)=>item.missionId !== paramsNum ? item : []);
+  );
   useEffect(() => {
-    setLoding(true)
+    setLoding(true);
     dispatch(__GetTodaymission());
     dispatch(__GetDailymission());
-    setLoding(false)
+    setLoding(false);
   }, [dispatch]);
-  // console.log(text)
   return (
     <>
       {!loding ? (
         <div className="detail-wrap-shape">
           <div className="mission-name-and-tag-area">
-            <div className="mission-name-text">{select ? select.missionName : 'MissionName'}</div>
+            <div className="mission-name-text">
+              {select ? select.missionName : 'MissionName'}
+            </div>
             <div className="mission-tag-text">#Mission Tag</div>
             <p className="explain-mission-contents-text">
-              {select ? select.missionContent : "내용이 들어갑니다"}
+              {select ? select.missionContent : '내용이 들어갑니다'}
             </p>
           </div>
 
-          <img className="mission-image-area" src={select ? select.missionImageUrl : "https://png.pngtree.com/thumb_back/fh260/background/20210207/pngtree-gray-solid-color-simple-background-image_557028.jpg"} />
+          <img
+            className="mission-image-area"
+            src={
+              select
+                ? select.missionImageUrl
+                : 'https://png.pngtree.com/thumb_back/fh260/background/20210207/pngtree-gray-solid-color-simple-background-image_557028.jpg'
+            }
+          />
           <button className="button-go-camera">
             <div className="button-go-camera-icon">
               <FiCamera

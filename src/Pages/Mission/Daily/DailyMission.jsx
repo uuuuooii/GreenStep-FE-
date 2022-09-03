0ddbda74e0e7f395440f44e891034937dc88982a
neo@zillinks.com <keunhey0107@gmail.com>
2022-09-03 23:03:08 +0900

    feat dailumission despatch

--- src/Pages/Mission/Daily/DailyMission.jsx
+++ src/Pages/Mission/Daily/DailyMission.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { DailyBody, MissionPhoto, MissionText } from "./DailyMissionStyled";
+//redux
+import { __GetDailymission } from "../../../Redux/modules/mission";
+import { useSelector, useDispatch } from "react-redux";
 
 const DailyMission = ({ item }) => {
+  //redux
+  const mission = useSelector((state) => state.mission.daily);
+  // console.log(mission);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(__GetDailymission());
+  }, [dispatch]);
+
   const navigate = useNavigate();
   return (
     <DailyBody onClick={() => navigate(`/missioncamera/${item.missionId}`)}>

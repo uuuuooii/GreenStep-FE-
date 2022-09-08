0906d783025788c297048afc2a066de5e3cd97ca
neo@zillinks.com <keunhey0107@gmail.com>
2022-09-08 23:59:00 +0900

    instance

--- src/Redux/modules/instance.js
+++ src/Redux/modules/instance.js
@@ -2,7 +2,6 @@ import axios from "axios";
 const URL = process.env.REACT_APP_URL;
 const instance = axios.create({
   baseURL: `https://greenstepserver.link/`,
-  // baseURL: `http://13.209.16.253:8080`,
 });
 instance.interceptors.request.use(function (instance) {
   const Token = localStorage.getItem("Authorization");

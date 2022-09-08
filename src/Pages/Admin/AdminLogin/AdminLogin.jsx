453ee16047377c07d72fe638b6ef52e4b508c771
neo@zillinks.com <keunhey0107@gmail.com>
2022-09-09 00:04:16 +0900

    fix

--- src/Pages/Admin/AdminLogin/AdminLogin.jsx
+++ src/Pages/Admin/AdminLogin/AdminLogin.jsx
@@ -1,13 +1,14 @@
 //react import
-import React from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import useInput from '../../../hooks/useInput';
+import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
+import useInput from "../../../hooks/useInput";
 //styled import
-import { Area } from './AdminLoginStyled';
+import { Area } from "./AdminLoginStyled";
+import instance from "../../../Redux/modules/instance";
 const AdminLogin = () => {
-  const [id, setid] = useInput('');
-  const [pw, setpw] = useInput('');
+  const [id, setid] = useInput("");
+  const [pw, setpw] = useInput("");
   const URL = process.env.REACT_APP_URL;
   const loginInfo = {
     email: id,
@@ -16,12 +17,12 @@ const AdminLogin = () => {
   const navigate = useNavigate();
 
   const Login = () => {
-    axios.post(`${URL}/admin/login`, loginInfo).then((res) => {
+    instance.post(`/admin/login`, loginInfo).then((res) => {
       const token = res.headers.authorization;
       const refresh_token = res.headers.refresh_token;
-      localStorage.setItem('Authorization', token);
-      sessionStorage.setItem('refresh-Token', refresh_token);
-      navigate('/admin');
+      localStorage.setItem("Authorization", token);
+      sessionStorage.setItem("refresh-Token", refresh_token);
+      navigate("/admin");
     });
   };
 

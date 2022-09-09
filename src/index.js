c9ddf82a139cef7df94341d91e8af93d67892891
neo@zillinks.com <keunhey0107@gmail.com>
2022-09-09 14:26:34 +0900

    pwa삭제

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux/es/exports";
-import store from './Redux/ConfigureStore'
+import store from "./Redux/ConfigureStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <Provider store={store} >
+  <Provider store={store}>
     <App />
-  {/* // </React.StrictMode> */}
+    {/* // </React.StrictMode> */}
   </Provider>
 );

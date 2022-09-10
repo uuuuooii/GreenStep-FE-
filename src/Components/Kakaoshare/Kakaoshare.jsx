d7e0e4a8f2683f04dac44cf3615b1294a703e1ee
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-09-10 20:40:29 +0900

    [delete]Slide

--- src/Components/Kakaoshare/Kakaoshare.jsx
+++ src/Components/Kakaoshare/Kakaoshare.jsx
@@ -53,16 +53,14 @@ const Kakaoshare = () => {
       });
     }
   };
-  console.log(123);
 
   return (
     <>
-      <div>test!!!</div>
       <div className="kakao-share-button">
         {/* Kakao share button */}
-        <button id="kakao-link-btn">
+        {/* <button id="kakao-link-btn">
           <img src="/icons/kakao.png" alt="kakao-share-icon" />
-        </button>
+        </button> */}
       </div>
     </>
   );

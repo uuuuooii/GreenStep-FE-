5c90852be3560f98c7e5beb462dc1e100b5c9fc5
neo@zillinks.com <keunhey0107@gmail.com>
2022-09-11 00:30:08 +0900

    test

--- src/Components/Kakaoshare/Kakaoshare.jsx
+++ src/Components/Kakaoshare/Kakaoshare.jsx
@@ -21,7 +21,7 @@ const Kakaoshare = () => {
         container: "#kakao-link-btn",
         objectType: "feed",
         content: {
-          title: "타이틀",
+          // title: "타이틀",
           description: "#리액트 #카카오 #공유버튼",
           imageUrl: "IMAGE_URL", // i.e. process.env.FETCH_URL + '/logo.png'
           link: {

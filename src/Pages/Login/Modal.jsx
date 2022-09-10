7707c5bc14aacac745bbc9e05037f8ce41b77479
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-09-11 03:13:44 +0900

    '[style]modal'

--- src/Pages/Login/Modal.jsx
+++ src/Pages/Login/Modal.jsx
@@ -1,28 +1,33 @@
 //react import
 import React, { useState, useEffect } from 'react';
 import useInput from '../../hooks/useInput';
-import { useDispatch,useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 //modules import
-import { getUserInfoThunk } from "../../Redux/modules/userInfoSlice";
+import { getUserInfoThunk } from '../../Redux/modules/userInfoSlice';
 import instance from '../../Redux/modules/instance';
 //component import
-import FirstModal from "./Modal/FirstModal/FirstModal";
-import SecondModal from "./Modal/SecondModal/SecondModal";
-import ThirdModal from "./Modal/ThirdModal/ThirdModal";
-import LoadingBar from "../../Components/LoadingBar/LoadingBar";
+import FirstModal from './Modal/FirstModal/FirstModal';
+import SecondModal from './Modal/SecondModal/SecondModal';
+import ThirdModal from './Modal/ThirdModal/ThirdModal';
+import LoadingBar from '../../Components/LoadingBar/LoadingBar';
 
 const Modal = () => {
   const [loading, setLoading] = useState(false);
   const [display, setDisplay] = useState(1);
-  const [img, setImg] = useState("");
+  const [img, setImg] = useState('');
   const [check, setCheck] = useState(0);
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.user);
 
   useEffect(() => {
     setLoading(true);
-instance.get('/users/info').then((res)=>{setName(res.data.data.name);setNickname(res.data.data.nickname);setImg(res.data.data.profilePhoto)})
+    dispatch(getUserInfoThunk());
     setLoading(false);
+    setImg(user.profilePhoto);
+    setName(user.name);
+    setNickname(user.nickname);
   }, []);
 
   return !loading ? (
@@ -33,7 +38,7 @@ instance.get('/users/info').then((res)=>{setName(res.data.data.name);setNickname
         setImg={setImg}
         check={check}
         setCheck={setCheck}
-
+        user={user}
       />
       <SecondModal
         display={display}

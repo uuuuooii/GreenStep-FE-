137bc9eb08f41bf3e8cf8d52c38d5330bce0f85a
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-09-13 00:25:53 +0900

    feat feed page fade on

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -25,39 +25,33 @@ const MyPageIcon = styled(MdPersonOutline)`
 export function Footer() {
   const navigate = useNavigate();
   const [page, setPage] = useState('Mission');
+  const SetFeed = () => {
+    setPage('Feed');
+    navigate('/feed');
+  };
+  const SetMission = () => {
+    setPage('Mission');
+    navigate('/Mission');
+  };
+  const SetMyPage = () => {
+    setPage('MyPage');
+    navigate('/myPage');
+  };
   return (
     <>
       <div className="footer-dummy-div"></div>
 
       <div className="footer-box">
         <div className="wrap-footer-icons">
-          <div
-            className="footer-icon-1"
-            onClick={() => {
-              setPage('Feed');
-              navigate('/Feed');
-            }}
-          >
+          <div className="footer-icon-1" onClick={() => SetFeed()}>
             <FeedIcon color={page === 'Feed' ? '#DAF2D5' : 'black'} />
           </div>
 
-          <div
-            className="footer-icon-2"
-            onClick={() => {
-              setPage('Mission');
-              navigate('/Mission');
-            }}
-          >
+          <div className="footer-icon-2" onClick={() => SetMission()}>
             <HomeIcon color={page === 'Mission' ? '#DAF2D5' : 'black'} />
           </div>
 
-          <div
-            className="footer-icon-3"
-            onClick={() => {
-              setPage('MyPage');
-              navigate('/MyPage');
-            }}
-          >
+          <div className="footer-icon-3" onClick={() => SetMyPage()}>
             <MyPageIcon color={page === 'MyPage' ? '#DAF2D5' : 'black'} />
           </div>
         </div>

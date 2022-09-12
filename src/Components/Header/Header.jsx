20304ab97af33e1562a3b41156260e01349d103e
Rita Soojin Woo <linawoo@naver.com>
2022-09-12 11:53:14 +0900

    [feat] Header 그린스텝 텍스트 클릭 시, 미션 페이지로 이동

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Header.css";
 import { BiBell } from "react-icons/bi";
 import HeaderLogo from "../../static/components/HeaderLogo";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="header-dummy-div"></div>
 
       <div className="wrap-header">
         <div className="header-relative">
-          <div className="header-title">Green Step</div>
+          <div className="header-title" onClick={() => navigate("/Mission")}>
+            Green Step
+          </div>
           <div className="header-icon-left">
             <HeaderLogo />
           </div>

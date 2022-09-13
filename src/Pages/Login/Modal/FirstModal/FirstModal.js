79531f8a2ade1e342ed5524f061dbf4508135958
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-09-14 02:20:11 +0900

    [feat]modal

--- src/Pages/Login/Modal/FirstModal/FirstModal.js
+++ src/Pages/Login/Modal/FirstModal/FirstModal.js
@@ -1,5 +1,5 @@
 //react import
-import React from "react";
+import React from 'react';
 //styled import
 import {
   FirstModalBody,
@@ -14,8 +14,8 @@ import {
   ButtonText,
   DummyText,
   SelectText,
-} from "./FirstModalStyled";
-import { useState } from "react";
+} from './FirstModalStyled';
+import { useState } from 'react';
 
 const FirstModal = ({
   setDisplay,
@@ -31,55 +31,60 @@ const FirstModal = ({
     setSlide(true);
     setNickname(user.nickname);
   };
-  return (
-<>
-        <ModalHeader>
-          <DummyText></DummyText>
-          <TopText>프로필 사진</TopText>
-          <ButtonText
-            onClick={() => {
-              check === 0 ? alert("선택해주세요") : Next();
-            }}
-            check={check}
-          >
-            다음
-          </ButtonText>
-        </ModalHeader>
-        <SelectBody slide={slide}>
-          <ImgArea>
-            <SeleceArea>
-              <ImgDiv>
-                <SelectImg
-                  src={
-                    user.profilePhoto
-                      ? user.profilePhoto
-                      : "https://blog.kakaocdn.net/dn/Sq4OD/btqzlkr13eD/dYwFnscXEA6YIOHckdPDDk/img.jpg"
-                  }
-                  onClick={() => {
-                    check === 1 ? setCheck(0) : setCheck(1);
-                    setImg(user.profilePhoto);
-                  }}
-                  check={check}
-                  num={1}
-                />
-                <SelectText>카카오톡 프로필</SelectText>
-              </ImgDiv>
 
-              <ImgDiv>
-                <SelectImg
-                  src="images/펭귄.png"
-                  onClick={() => {
-                    check === 2 ? setCheck(0) : setCheck(2);
-                  }}
-                  check={check}
-                  num={2}
-                />
-                <SelectText>프로필 캐릭터 선택</SelectText>
-              </ImgDiv>
-            </SeleceArea>
-          </ImgArea>
-        </SelectBody>
-</>
+  return (
+    <>
+      <ModalHeader>
+        <DummyText></DummyText>
+        <TopText>프로필 사진</TopText>
+        <ButtonText
+          onClick={() => {
+            check === 0 ? alert('선택해주세요') : Next();
+          }}
+          check={check}
+        >
+          다음
+        </ButtonText>
+      </ModalHeader>
+      <SelectBody slide={slide}>
+        <ImgArea>
+          <SeleceArea>
+            <ImgDiv>
+              <SelectImg
+                src={
+                  user.profilePhoto
+                    ? user.profilePhoto
+                    : 'https://blog.kakaocdn.net/dn/Sq4OD/btqzlkr13eD/dYwFnscXEA6YIOHckdPDDk/img.jpg'
+                }
+                onClick={() => {
+                  check === 1 ? setCheck(0) : setCheck(1);
+                  !user.profilePhoto
+                    ? setImg(
+                        'https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'
+                      )
+                    : setImg(user.profilePhoto);
+                }}
+                check={check}
+                num={1}
+              />
+              <SelectText>카카오톡 프로필</SelectText>
+            </ImgDiv>
+
+            <ImgDiv>
+              <SelectImg
+                src="images/펭귄.png"
+                onClick={() => {
+                  check === 2 ? setCheck(0) : setCheck(2);
+                }}
+                check={check}
+                num={2}
+              />
+              <SelectText>프로필 캐릭터 선택</SelectText>
+            </ImgDiv>
+          </SeleceArea>
+        </ImgArea>
+      </SelectBody>
+    </>
   );
 };
 

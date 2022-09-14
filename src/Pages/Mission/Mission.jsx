87fe383d0070559fbb910fd97e4730fdd88d82ba
neo@zillinks.com <keunhey0107@gmail.com>
2022-09-15 01:17:08 +0900

    카톡공유버튼 이동

--- src/Pages/Mission/Mission.jsx
+++ src/Pages/Mission/Mission.jsx
@@ -17,7 +17,7 @@ import DailyChallenge from "./Daily/DailyChallenge";
 import Footer from "../../Components/Footer/Footer";
 import ChallengeSkeleton from "../../Components/Skeleton/ChallengeSkeleton";
 import DailySkeleton from "../../Components/Skeleton/DailySkeleton";
-
+import Kakaoshare from "../../Components/Kakaoshare/Kakaoshare";
 //styled import
 import {
   DailyMissionArea,
@@ -81,6 +81,7 @@ const Mission = ({ Header }) => {
           <DailyMissionArea>
             <DailyTextArea>
               <DailyText>데일리 미션</DailyText>
+              <Kakaoshare />
             </DailyTextArea>
             <DailyCardBox>
               {!loading && missionDaily ? (

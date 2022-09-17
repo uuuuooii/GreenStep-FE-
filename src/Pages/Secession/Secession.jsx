2e426b2e0997ddef8cd768efd06c1153a5646792
neo@zillinks.com <keunhey0107@gmail.com>
2022-09-18 06:24:40 +0900

    style:secession

--- src/Pages/Secession/Secession.jsx
+++ src/Pages/Secession/Secession.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import "./Secession.css";
+import Secessionimg from "../../static/components/Secessionimg";
 
 const Secession = () => {
-  return <div className="archive-top-button">secession</div>;
+  return (
+    <>
+      <div className="secession-settings-button-area"></div>
+      <div className="secessiontitle">
+        그린스텝과 지구를 지키는 일을 중단하실 건가요?
+      </div>
+      <Secessionimg />
+      <button className="secessionbutton">
+        네, 저 혼자 지구를 지키겠습니다
+      </button>
+    </>
+  );
 };
 
 export default Secession;

d0ff04e37921ac7ed73313cdd3a25df844271287
Rita Soojin Woo <linawoo@naver.com>
2022-09-18 18:52:39 +0900

    [feat] modal 기능 test

--- src/Pages/MyPage/MyPage/ViewMoreModal/ViewMoreModal.jsx
+++ src/Pages/MyPage/MyPage/ViewMoreModal/ViewMoreModal.jsx
@@ -1,10 +1,45 @@
 import React from "react";
 import "./ViewMoreModal.css";
+import { useState } from "react";
 
 const ViewMoreModal = () => {
+  const [modal, setModal] = useState(false);
+
   return (
     <>
-      <div>ViewMoreModal</div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde aliquam
+        iure voluptate in libero provident laudantium, perferendis, similique
+        nemo sapiente id. Quam vel nisi minus optio accusamus architecto iusto
+        alias?
+        <button
+          onClick={() => {
+            setModal(true);
+          }}
+        >
+          go modal
+        </button>
+        {modal ? (
+          <>
+            <div
+              className="modal-background"
+              onClick={() => {
+                setModal(false);
+              }}
+            ></div>
+            <div class="modal-box">
+              Hello, this is modal window
+              <button
+                onClick={() => {
+                  setModal(false);
+                }}
+              >
+                X
+              </button>
+            </div>
+          </>
+        ) : null}
+      </div>
     </>
   );
 };

1e704ee394208456e8fa9ed77eecc6c9e3667a55
Rita Soojin Woo <linawoo@naver.com>
2022-09-19 06:32:50 +0900

    merged

--- src/Pages/MyPage/MyPage/MyPage.jsx
+++ src/Pages/MyPage/MyPage/MyPage.jsx
@@ -1,21 +1,21 @@
 //react import
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 //modules import
 import {
   getUserInfoThunk,
   getCertThunk,
   getPostThunk,
-} from '../../../Redux/modules/userInfoSlice';
+} from "../../../Redux/modules/userInfoSlice";
 //component import
-import Footer from '../../../Components/Footer/Footer';
-import Header from '../../../Components/Header/Header';
-import ProfilePencil from '../../../static/components/ProfilePencil';
+import Footer from "../../../Components/Footer/Footer";
+import Header from "../../../Components/Header/Header";
+import ProfilePencil from "../../../static/components/ProfilePencil";
 //styled import
 import "./MyPage.css";
 import styled from "styled-components";
-import { FadeOn,SlideBottom } from "../../Feed/FeedStyled";
+import { FadeOn, SlideBottom } from "../../Feed/FeedStyled";
 import { HiDotsHorizontal } from "react-icons/hi";
 import { ArchiveArrow } from "./Archive/ArchiveStyled";
 import ViewMoreRowBar from "../../../static/components/ViewMoreRowBar";
@@ -39,7 +39,7 @@ const MyPageModal = styled.div`
   animation-duration: 0.5s;
   animation-timing-function: ease-out;
   animation-fill-mode: forwards;
-`
+`;
 
 const MyPageDiv = styled.div`
   display: flex;
@@ -63,10 +63,10 @@ const MyPage = () => {
   const post = useSelector((state) => state.userInfo.post);
 
   const FirstText = `아직 ${
-    userInfo.nickname ? userInfo.nickname : '@@@'
+    userInfo.nickname ? userInfo.nickname : "@@@"
   }님의 흔적이 보이지 않아요 🥲 \n 지구를 향한 그린 스텝 보여주세요!`;
   const SecondText = `다른 이도 ${
-    userInfo.nickname ? userInfo.nickname : '@@@'
+    userInfo.nickname ? userInfo.nickname : "@@@"
   }님이 그린 스텝을 보고싶어해요! 🤩 \n 피드에 공유해 주세요!`;
   useEffect(() => {
     setLoding(true);
@@ -97,13 +97,8 @@ const MyPage = () => {
                 setViewMoreModal(false);
               }}
             ></div>
-<<<<<<< HEAD
-            {/* <Slide bottom> */}
-            <div class="mypage-modal-box">
-=======
 
             <MyPageModal>
->>>>>>> Dev
               <div
                 onClick={() => {
                   setViewMoreModal(false);
@@ -136,12 +131,7 @@ const MyPage = () => {
                   <div className="mypage-modal-logout-text">로그아웃</div>
                 </div>
               </div>
-<<<<<<< HEAD
-            </div>
-            {/* </Slide> */}
-=======
             </MyPageModal>
->>>>>>> Dev
           </>
         ) : null}
 
@@ -152,7 +142,7 @@ const MyPage = () => {
                 <img src={userInfo.profilePhoto} className="image-area" />
                 <div
                   className="image-pencil-div"
-                  onClick={() => navigate('/updatemypage')}
+                  onClick={() => navigate("/updatemypage")}
                 >
                   <ProfilePencil />
                 </div>
@@ -160,10 +150,10 @@ const MyPage = () => {
 
               <div className="nick-and-email-area">
                 <div className="nickname-text">
-                  {userInfo.nickname ? userInfo.nickname : 'Nickname'}
+                  {userInfo.nickname ? userInfo.nickname : "Nickname"}
                 </div>
                 <div className="email-text">
-                  {userInfo.email ? userInfo.email : 'Email'}
+                  {userInfo.email ? userInfo.email : "Email"}
                 </div>
               </div>
             </div>
@@ -172,10 +162,10 @@ const MyPage = () => {
           <div className="image-nick-email">
             <div className="image-area"></div>
             <div className="nickname-text">
-              {userInfo.nickname ? userInfo.nickname : 'Nickname'}
+              {userInfo.nickname ? userInfo.nickname : "Nickname"}
             </div>
             <div className="email-text">
-              {userInfo.email ? userInfo.email : 'Email'}
+              {userInfo.email ? userInfo.email : "Email"}
             </div>
           </div>
         )}
@@ -187,7 +177,7 @@ const MyPage = () => {
                 <div className="photoshots-text">인증샷 아카이브</div>
                 <div className="photoshots-viewmore-icon">
                   <ArchiveArrow
-                    onClick={() => navigate('/archive/certification')}
+                    onClick={() => navigate("/archive/certification")}
                   />
                 </div>
               </div>
@@ -234,7 +224,7 @@ const MyPage = () => {
               <div className="posts-text-and-icon">
                 <div className="posts-text">게시물 아카이브</div>
                 <div className="posts-viewmore-icon">
-                  <ArchiveArrow onClick={() => navigate('/archive/post')} />
+                  <ArchiveArrow onClick={() => navigate("/archive/post")} />
                 </div>
               </div>
             ) : (

d5fcf6ba1d1e88d1cd0c920df965b0af280a1c30
Rita Soojin Woo <linawoo@naver.com>
2022-09-22 07:07:39 +0900

    [style] 폰트 적용 및 헤더/ 인풋창/ 버튼 디테일, 그림자 스타일, 폰트 색상 변경

--- src/Pages/Login/Modal/FourthModal/FourthModalStyled.js
+++ src/Pages/Login/Modal/FourthModal/FourthModalStyled.js
@@ -1,24 +1,12 @@
 import styled, { keyframes } from "styled-components";
 
-import { AiOutlineCheckCircle,AiFillCheckCircle } from "react-icons/ai";
+import { AiOutlineCheckCircle, AiFillCheckCircle } from "react-icons/ai";
 
 const SlideRight = keyframes`
 from{transform:translateX(300px)}
 to{transform:translateX(0px)}
 `;
 
-
-export const ButtonText = styled.div`
-  color: #868686;
-  position: absolute;
-  left: 0;
-  /* margin: auto; */
-  width: 32px;
-  margin: 0 10px;
-  &:hover {
-    cursor: pointer;
-  }
-`;
 export const ModalHeader = styled.div`
   position: relative;
   width: 100%;
@@ -26,35 +14,54 @@ export const ModalHeader = styled.div`
   justify-content: center;
   text-align: center;
   display: flex;
-  background-color: white;
+  background: rgba(255, 255, 255, 0.5);
+  border-bottom: 2.5px solid rgba(218, 218, 218, 0.15);
 `;
+
+export const ButtonText = styled.div`
+  position: absolute;
+  left: 0;
+  width: 32px;
+  font-family: "AppleNeoR";
+  font-size: 18px;
+  margin: 3px 15px 2px 15px;
+  color: #868686;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 export const TopText = styled.div`
   text-align: center;
-  margin: 0 auto;
-  color: #036a6a;
+  font-family: "AppleNeoR";
+  font-style: normal;
   font-weight: 600;
   font-size: 20px;
-  line-height: 24px;
+  color: #50674c;
+  margin: 2px auto;
 `;
+
 export const SelectBody = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  margin-top: 150px;
+  /* justify-content: center; */
   animation-name: ${SlideRight};
   animation-duration: 0.5s;
   animation-timing-function: ease-out;
   animation-fill-mode: forwards;
   overflow-y: scroll;
   overflow-x: hidden;
-  background-color: #FCFCFA;
+  background-color: #fcfcfa;
 
   &::-webkit-scrollbar {
     display: none;
   }
 `;
+
 export const TotalEmailArea = styled.div`
   display: flex;
   align-items: center;
@@ -69,6 +76,7 @@ export const CheckMailArea = styled.div`
     cursor: pointer;
   }
 `;
+
 export const CheckedMailIcon = styled(AiFillCheckCircle)`
   color: #d9d9d9;
   width: 20px;
@@ -76,9 +84,9 @@ export const CheckedMailIcon = styled(AiFillCheckCircle)`
   font-size: 20px;
   transition: all 0.5s;
   border-radius: 10px;
-`
-export const CheckMailIcon = styled(AiOutlineCheckCircle)`
+`;
 
+export const CheckMailIcon = styled(AiOutlineCheckCircle)`
   color: #d9d9d9;
   width: 20px;
   height: 20px;
@@ -88,31 +96,36 @@ export const CheckMailIcon = styled(AiOutlineCheckCircle)`
 `;
 
 export const CheckMailText = styled.div`
+  font-family: "AppleNeoR";
   font-size: 14px;
   margin: 0 7px;
-  color: ${(props)=>props.color};
-  color: #d9d9d9;
+  color: ${(props) => props.color};
+  color: #c3c2c2;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
+
 export const EmailButton = styled.button`
-  background: ${(props)=>props.background};
-  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.25);
+  background: ${(props) => props.background};
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.25);
+  font-family: "AppleNeoSB";
+  font-size: 16px;
   border-radius: 3px;
   width: 110px;
   height: 33px;
   border: none;
-  color: #FCFCFA;
+  color: #fcfcfa;
   position: absolute;
   transition: all 0.5s;
   &:hover {
     cursor: pointer;
   }
 `;
+
 export const EmailStrong = styled.div`
-  color: #036a6a;
-  font-style: normal;
+  color: #50674c;
+  font-family: "AppleNeoM";
   font-weight: 700;
   font-size: 15px;
   line-height: 18px;
@@ -121,17 +134,19 @@ export const EmailStrong = styled.div`
   text-align: center;
   margin: 10px 0;
 `;
+
 export const EmailP = styled.p`
-  width: 90%;
-  font-style: normal;
+  width: 100%;
+  font-family: "AppleNeoR";
   font-weight: 500;
   font-size: 13px;
   line-height: 16px;
   display: flex;
   align-items: center;
   text-align: center;
-  color: #036a6a;
+  color: #50674c;
 `;
+
 export const EmailButtonArea = styled.div`
   position: relative;
   display: flex;

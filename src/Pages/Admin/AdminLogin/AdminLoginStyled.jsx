2d6bf84db740f5b630dc859c8b8e4ebbf2b64f45
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-09-22 15:08:13 +0900

    git cash

--- src/Pages/Admin/AdminLogin/AdminLoginStyled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Area = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 200px;
-    justify-content:space-between;
-`
--- src/Pages/Mission/Camera/CameraBar.jsx
+++
@@ -1,8 +0,0 @@
-//styled import
-import styled from 'styled-components';
-
-export const UploadImg = styled.img`
-  width: 350px;
-  height: 350px;
-  margin: 90px 0;
-`;

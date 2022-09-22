9354db99754086867173314d95fa5e43c2ea73a7
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-09-22 15:01:40 +0900

    fix commit

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import Modal from './Pages/Login/Modal';
 import MissionCamera from './Pages/Mission/Camera/MissionCamera';
 import Loding from './Pages/Loding/Loding';
 import Error from './Pages/Error/Error';
-import ExplainWaiting from './Pages/Mission/Explain/ExplainWating';
+import ExplainWaiting from './Pages/Mission/Explain/ExplainWaiting';
 import instance from './Redux/modules/instance';
 import Kakaoshare from './Components/Kakaoshare/Kakaoshare';
 import SetPullToRefresh from './Components/PullToRefresh/SetPullToRefresh';

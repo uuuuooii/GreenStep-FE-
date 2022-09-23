320f1eab4796b0e3bd4c0cef0407278f0902f406
neo@zillinks.com <keunhey0107@gmail.com>
2022-09-23 10:37:14 +0900

    ㅅㄷㄴㅅ

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { Provider } from "react-redux/es/exports";
 import store from "./Redux/ConfigureStore";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,3 +17,8 @@ root.render(
 );
 
 serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);

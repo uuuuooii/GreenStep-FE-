988d2648a3ee7faec551b2e4acd1b84545216dc3
Rita Soojin Woo <linawoo@naver.com>
2022-09-25 05:54:13 +0900

    [style] 디테일 수정, 색상 변경, 재활용 하던 div 세분화

--- src/Pages/Mission/Upload/UploadStyled.jsx
+++ src/Pages/Mission/Upload/UploadStyled.jsx
@@ -59,18 +59,18 @@ export const UploadContentTextArea = styled.textarea`
   padding: 10px;
   border-radius: 5px;
   border: none;
-  font-family: "AppleNeoM";
+  font-family: "AppleNeoR";
   font-size: 13px;
   font-weight: 600;
   line-height: 16px;
   &::placeholder {
-    background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg' %3E%3Cpath d='M0 12.375V15.5H3.125L12.3417 6.28333L9.21667 3.15833L0 12.375ZM14.7583 3.86667C15.0833 3.54167 15.0833 3.01667 14.7583 2.69167L12.8083 0.741667C12.4833 0.416667 11.9583 0.416667 11.6333 0.741667L10.1083 2.26667L13.2333 5.39167L14.7583 3.86667Z' fill='%23B2E2AB' /%3E%3C/svg%3E");
+    background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg' %3E%3Cpath d='M0 12.375V15.5H3.125L12.3417 6.28333L9.21667 3.15833L0 12.375ZM14.7583 3.86667C15.0833 3.54167 15.0833 3.01667 14.7583 2.69167L12.8083 0.741667C12.4833 0.416667 11.9583 0.416667 11.6333 0.741667L10.1083 2.26667L13.2333 5.39167L14.7583 3.86667Z' fill='%232b5139' /%3E%3C/svg%3E");
     background-size: contain;
     background-position: 1px center;
     background-repeat: no-repeat;
-    background-position-x: 215px;
-    color: #84ca79;
-    font-family: "AppleNeoM";
+    background-position-x: 185px;
+    color: #2b5139;
+    font-family: "AppleNeoR";
     font-weight: 600;
     font-size: 13px;
     line-height: 16px;
@@ -104,12 +104,32 @@ export const KakaoReactIcon = styled(RiKakaoTalkFill)`
   font-size: 25px;
   background-color: transparent;
 `;
+
+export const UploadIconDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-left: 15px;
+  margin-top: 2px;
+`;
+
+export const KakaoIconDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-left: 2px;
+`;
+
 export const IconDiv = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  margin-left: 3px;
 `;
+
 export const ButtonText = styled.div`
   display: flex;
   flex-direction: column;
@@ -129,11 +149,12 @@ export const ShareText = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-family: "AppleNeoM";
+  font-family: "AppleNeoR";
   font-weight: 600;
   font-size: 15px;
   line-height: 18px;
-  margin-left: 5px;
+  margin-left: 3px;
+  margin-bottom: 1px;
   height: 100%;
 `;
 export const TotalButtonArea = styled.div`

6bb0d9adb8f4f51fb23a07674dd25ccd547fa382
neo@zillinks.com <keunhey0107@gmail.com>
2022-09-27 01:40:23 +0900

    test

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,13 +1,13 @@
 //react import
-import React, { useEffect, useState } from 'react';
-import { useRef } from 'react';
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
 import {
   LogoFade,
   FadeBottomOn,
   FadeBottomOnImage,
-} from '../../Components/Animation/Animation';
-import { useInView } from 'react-intersection-observer';
-import LoginBody1 from '../../static/components/LoginBody1';
+} from "../../Components/Animation/Animation";
+import { useInView } from "react-intersection-observer";
+import LoginBody1 from "../../static/components/LoginBody1";
 //styled import
 import {
   LoginPageArea,
@@ -48,10 +48,10 @@ const cloudimageConfigWithCustomCNAMEDomain = {
 };
 
 const text1 =
-  ' 당신의 작지만 일상적인 \n 환경 미션을 통해서,\n 나비 효과를 만들어\n 아주 큰 변화로 이끌어보세요.';
+  " 당신의 작지만 일상적인 \n 환경 미션을 통해서,\n 나비 효과를 만들어\n 아주 큰 변화로 이끌어보세요.";
 const text2 =
-  '무엇부터 시작하죠? \n 일상생활에서 실천 가능한\n 다양한 미션들을 받아보세요';
-const text3 = '다른 사람들의 인증샷을 보고 \n 동기부여를 받아보세요.';
+  "무엇부터 시작하죠? \n 일상생활에서 실천 가능한\n 다양한 미션들을 받아보세요";
+const text3 = "다른 사람들의 인증샷을 보고 \n 동기부여를 받아보세요.";
 const text4 = '당신이 그릴 스텝, \n "내가 그린 스텝" \n 지금 시작해볼까요?';
 const Login = () => {
   const [first, firstView] = useInView();
@@ -77,7 +77,6 @@ const Login = () => {
   // useEffect(()=>{},[secondView])
 
   return (
-
     <>
       <LoginPageArea>
         <LogoArea>
@@ -169,8 +168,8 @@ const Login = () => {
           </LastText>
         </FourthArea>
         <MarginDIv>
-          <KakaoLink href="https://kauth.kakao.com/oauth/authorize?client_id=3e7b3b7d9b0de5387c8ac0d9325f57ab&redirect_uri=https://greenstepapp.com/users/kakao/callback&response_type=code">
-            {/* <KakaoLink href="https://kauth.kakao.com/oauth/authorize?client_id=3e7b3b7d9b0de5387c8ac0d9325f57ab&redirect_uri=http://localhost:3000/users/kakao/callback&response_type=code"> */}
+          {/* <KakaoLink href="https://kauth.kakao.com/oauth/authorize?client_id=3e7b3b7d9b0de5387c8ac0d9325f57ab&redirect_uri=https://greenstepapp.com/users/kakao/callback&response_type=code"> */}
+          <KakaoLink href="https://kauth.kakao.com/oauth/authorize?client_id=3e7b3b7d9b0de5387c8ac0d9325f57ab&redirect_uri=http://localhost:3000/users/kakao/callback&response_type=code">
             <ButtonBody>
               <Logo />
             </ButtonBody>
@@ -178,7 +177,6 @@ const Login = () => {
         </MarginDIv>
       </LoginPageArea>
     </>
-
   );
 };
 

1a5b9c8fa22c3e8d30f2752fcb2cc79c481b3b45
Rita Soojin Woo <linawoo@naver.com>
2022-09-28 08:34:16 +0900

    none

--- src/Pages/MyPage/MyPage/Archive/Archive.jsx
+++ src/Pages/MyPage/MyPage/Archive/Archive.jsx
@@ -33,7 +33,7 @@ import {
   DeleteCancelButton,
   ModalArea,
   ArchivePage,
-} from './ArchiveStyled';
+} from "./ArchiveStyled";
 
 const Archive = () => {
   const param = useParams().id;
@@ -176,8 +176,6 @@ const Archive = () => {
           </div>
         ) : null}
       </WrapArchive>
-
-
 
       {modal ? (
         <ModalArea>

465007f40a43430106d1e293632600037434856d
Rita Soojin Woo <linawoo@naver.com>
2022-10-06 22:08:37 +0900

    [docs] 이벤트 배너 제거

--- src/Pages/Mission/Mission.jsx
+++ src/Pages/Mission/Mission.jsx
@@ -51,11 +51,11 @@ const Mission = ({ onClickToast }) => {
         <>
           {!loading && missionChallenge ? (
             <>
-              <div className="mission-event-image">
+              {/* <div className="mission-event-image">
                 <a href="https://forms.gle/vXFMpi7GF2dFGVPS8">
                   <EventImage />
                 </a>
-              </div>
+              </div> */}
               <div
                 className="mission-notice-area"
                 onClick={() => navigate("/notice")}

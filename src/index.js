17448d555073ad1624800286730c0a52b9f4a851
neo@zillinks.com <keunhey0107@gmail.com>
2022-10-06 17:28:34 +0900

    reportWebVitals

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ root.render(
   </Provider>
 );
 
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
